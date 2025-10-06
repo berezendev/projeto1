8185759e2fb9f5a16555fcc1579cecfb904e2b7c
berezendev <bevogasrezende@gmail.com>
2025-10-06 15:25:21 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,3 +1,10 @@
 import streamlit as st
 
-st.write('Olá, mundo!')
+st.title('Meu programa')
+st.write('Olá, mundo!')]
+
+nome=st.text_input("Digite o seu nome")
+if nome:
+  st.write(nome.upper())
+
+
